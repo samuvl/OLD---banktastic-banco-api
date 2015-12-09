0981244f28362d1dc6f377479314c6a1712efd50
Your Name <you@example.com>
2015-12-09 16:59:18 +0100

    IMPL mayus json

--- src/java/com/fpmislata/banco/presentacion/json/IMPLmayusculas/JsonTransformerImplJackson.java
+++ src/java/com/fpmislata/banco/presentacion/json/IMPLmayusculas/JsonTransformerImplJackson.java
@@ -1,4 +1,4 @@
-package com.fpmislata.banco.presentacion.json.impl;
+package com.fpmislata.banco.presentacion.json.IMPLmayusculas;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
