10f13ea7f97a05ba817c0121bf11590b10a0b38b
Your Name <you@example.com>
2015-12-09 17:07:01 +0100

    PUTAS MAYUSCULAS HIJAS DE PUTA DE IMPL DAO Y JSON

--- src/java/com/fpmislata/banco/presentacion/json/implementacion/JsonTransformerImplJackson.java
+++ src/java/com/fpmislata/banco/presentacion/json/implementacion/JsonTransformerImplJackson.java
@@ -1,4 +1,4 @@
-package com.fpmislata.banco.presentacion.json.impl;
+package com.fpmislata.banco.presentacion.json.implementacion;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
